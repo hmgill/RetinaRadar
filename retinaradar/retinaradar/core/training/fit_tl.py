77d0b21fabf5bbe1b2c6df13705011118168c2ff
Hunter Mathias Gill <66973697+hmgill@users.noreply.github.com>
2025-11-03 19:26:58 -0500

    Update fit_tl.py
    Signed-off-by: Hunter Mathias Gill <66973697+hmgill@users.noreply.github.com>

--- retinaradar/retinaradar/core/training/fit_tl.py
+++ retinaradar/retinaradar/core/training/fit_tl.py
@@ -1,10 +1,12 @@
-
 """
 
 
 """
+import dill
+import json 
 import mlflow 
 import pytorch_lightning as pl
+from pathlib import Path
 from pytorch_lightning.loggers import MLFlowLogger
 
 from retinaradar.core.io import IO
@@ -42,6 +44,26 @@ class FitTL:
             """
             
             dataset = self.io.read_dataset()
+            # shuffle
+            dataset.shuffle(seed=42)
+            print(dataset.get_shuffle_info())
+
+            '''
+            dataset.compute_and_store_onehot_encodings()
+
+            with open('dataset.dill', 'wb') as f:
+                dill.dump(dataset, f)
+
+            for datapoint in dataset.datapoints[:10]:
+                l = datapoint.onehot_encoded_array
+
+                l2 = dataset.decode_onehot_to_original(l)
+
+                print(l)
+                print(l2)
+                print("\n")
+            '''
+            
             
             # convert dataset to data module
             data_module = RetinaRadarDataModule(
@@ -60,10 +82,14 @@ class FitTL:
             # get model name
             model_name = self.config["tl"]["model"]["name"]
             
+            # get label names from the one-hot encoder
+            label_names = dataset.onehot_encoder.get_feature_names_out().tolist()
+            
             model = MultiLabelImageClassifier(
                 num_labels = num_labels,
                 model_name = model_name,
-                learning_rate = self.config["tl"]["fit"]["hyperparameters"]["lr"]
+                learning_rate = self.config["tl"]["fit"]["hyperparameters"]["lr"],
+                label_names = label_names
             )            
 
 
@@ -71,6 +97,7 @@ class FitTL:
             TRAIN TL MODEL
             """
 
+            
             # --- 4. Training ---
             trainer = pl.Trainer(
                 max_epochs = self.config["tl"]["fit"]["hyperparameters"]["max_epochs"],
@@ -79,23 +106,42 @@ class FitTL:
                 logger = mlflow_logger
             )
     
-            trainer.fit(
-                model,
-                datamodule = data_module
-            )
+            #trainer.fit(
+            #    model,
+            #    datamodule = data_module
+            #)
             
 
+            """
+            SAVE MODEL METADATA FOR INFERENCE
+            """
+            # Extract metadata from dataset
+            metadata = dataset.get_metadata_for_inference()
+            
+            # Save metadata as JSON artifact
+            metadata_path = Path("label_metadata.json")
+            with open(metadata_path, 'w+') as f:
+                json.dump(metadata, f, indent=2)
+            
+            mlflow.log_artifact(str(metadata_path))
+            
+            # Also log as parameters for easy viewing in MLflow UI
+            mlflow.log_param("num_labels", metadata['num_labels'])
+            mlflow.log_param("feature_names", metadata['feature_names'])
+
+            
             """
             SAVE TL MODEL ARTIFACT
             """
 
+            '''
             # set registered model name
             registered_model_name = f"{model_name}_{run_id}"
             
             mlflow.pytorch.log_model(
                 pytorch_model = model,
                 registered_model_name = registered_model_name,
-                artifact_path = PATHS["retinaradar_tl_model"] 
+                artifact_path = PATHS["retinaradar_tl_model_efficientnet"] 
             )
-
             
+            '''
